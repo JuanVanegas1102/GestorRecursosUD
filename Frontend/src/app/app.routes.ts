a5d0d9ee951551281b598c9f4fff613897b966f5
JuanEstebanVanegas <juanesvanegas1102@gmail.com>
2024-05-19 03:25:36 -0500

    Primer avance Frontend
    Se creo el nav bar, slider principal y campo de categorias disponibles.

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',component: PaginaPrincipalComponent}
+];
